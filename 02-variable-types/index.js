5b7b5c8e9dd6035373ac1df6708fde2186c0f64f
bohdan992302 <bohdandidus@gmail.com>
2020-07-20 00:28:09 +0300

    some changes js

--- 02-variable-types/index.js
+++ 02-variable-types/index.js
@@ -1,30 +1,27 @@
 let userN = +prompt (`Введіть ціле число N`);
-    while(!Number.isInteger(userN)) {
-        userN = +prompt(`Число N не є цілим спробуйте ще раз`); 
-    }  
-    console.log('Число N:  ' + userN);
 
-let userM = +prompt (`Введіть ціле число M`);
+while(!Number.isInteger(userN) || userN ===0) {
+    userN = +prompt(`Число N не є цілим спробуйте ще раз`); 
+}  
+console.log('Число N:  ' + userN);
 
-    while (!Number.isInteger(userM) || (userN > userM)) {
-        if (userN > userM) {
-                userM = +prompt(`Число M є менше N `); 
-                 } else {
-                    userM = +prompt(`Число M не є цілим спробуйте ще раз`); 
-                 }
-     }       
-        console.log('Число M:  ' + userM);
+let userM = +prompt (`Введіть ціле число M більше за N`);
+
+while (!Number.isInteger(userM) || (userN > userM) ) {
+    (userN > userM) 
+        ? userM = +prompt(`Число M є менше N `)
+        : userM = +prompt(`Число M не є цілим спробуйте ще раз`)         
+}   
+console.log('Число M:  ' + userM);
 
 
 let agreePairValue = confirm('Пропускати парні числа?');
-    console.log("Пропускати парні?  " + agreePairValue);
+console.log('Пропускати парні?  ' + agreePairValue);
 
 
 let sumValue = 0;
 for (let i = userN; i <= userM; i++) {
-    
     if (agreePairValue && i%2 === 0)  continue;
-        sumValue += i; 
-    
+    sumValue += i; 
 }
 console.log('Сума чисел:  ' + sumValue);
