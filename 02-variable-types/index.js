654e781195dd393b089232ad78a427b46070a663
bohdan992302 <bohdandidus@gmail.com>
2020-07-21 02:05:24 +0300

    fix to parseInt

--- 02-variable-types/index.js
+++ 02-variable-types/index.js
@@ -1,16 +1,21 @@
-let userN = +prompt (`Введіть ціле число N`);
+let userN = prompt (`Введіть ціле число N`);
+let N = parseInt(userN);
 
-while(!Number.isInteger(userN) || userN ===0) {
-    userN = +prompt(`Число N не є цілим спробуйте ще раз`); 
-}  
-console.log('Число N:  ' + userN);
+while(N != userN || isNaN(N)){
+    alert('Це не число');
+    userN = prompt(`Введіть ціле число N`);
+    N = parseInt(userN);   
+}   
+console.log('Число N:  ' + N);
 
-let userM = +prompt (`Введіть ціле число M більше за N`);
+let userM = prompt (`Введіть ціле число M більше за ${N}`);
+let M = parseInt(userM);
 
-while (!Number.isInteger(userM) || (userN > userM) ) {
-    (userN > userM) 
-        ? userM = +prompt(`Число M є менше N `)
-        : userM = +prompt(`Число M не є цілим спробуйте ще раз`)         
+while (M != userM || (N > M) || isNaN(M)) {
+    (N > M) 
+        ? userM = prompt(`Число M є менше N ${N}`)
+        : userM = prompt(`Число M не є цілим спробуйте ще раз`);
+        M = parseInt(userM);
 }   
 console.log('Число M:  ' + userM);
 
