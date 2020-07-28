c26dcc89d6673976d233c53f9399e006fce25141
bohdan992302 <bohdandidus@gmail.com>
2020-07-29 00:24:33 +0300

    some issues HW4.js

--- 04-arrays/index.js
+++ 04-arrays/index.js
@@ -9,23 +9,24 @@ function getPairs(name) {
     let pairs = [];
 
     for (i = 0; i < name.length; i++) {
-        (name[i].charAt(name[i].length - 1) == "а") ? female.push(name[i]) : male.push(name[i]);  
+        (name[i].charAt(name[i].length - 1) == "а")  
+                                                ? female.push(name[i])  
+                                                : male.push(name[i]);  
     }
         for (j = 0; j < female.length; j++) {
             pairs[j]= [];
-            
-            pairs[j].push(male[j], female[j]);
+            pairs[j].push(male[j] + " i " + female[j]);
         }
     return pairs;  
 }
-const studensPairs = getPairs(students) 
-console.log(studensPairs)
+const studensPairs = getPairs(students);
+console.log(studensPairs);
 
 const studentsSubject = (pair, theme) => {
     for (i = 0; i < pair.length; i++) {
         pair[i].push(theme[i]);
-       }
-       return pair;
+    }
+    return pair;
 }
 const studentsTheme = studentsSubject(getPairs(students), themes);
 console.log(studentsTheme);
@@ -34,16 +35,16 @@ console.log(studentsTheme);
 const studentMarks = (student, value) => {
     let array = [];
     for (i = 0; i < student.length; i++) {
-        array[i] = [student[i],value[i]] 
+        array[i] = [student[i], value[i]]; 
     } 
-     return array;   
+    return array;   
 }
 console.log(studentMarks(students, marks));
 
 const randomMarksForProject = (unitedAll) => {
     for (i = 0; i < unitedAll.length; i++) {
-        let random = Math.floor(Math.random()* 5 + 1);
-        unitedAll[i].push(random)
+        let random = Math.floor(Math.random() * 5 + 1);
+        unitedAll[i].push(random);
     }
     return unitedAll;
 }
